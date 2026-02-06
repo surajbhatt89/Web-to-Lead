9315888cc6270c2c85d5d285ad5d38e829a0160c
Suraj Bhatt <surajbhatt8954@gmail.com>
2026-02-06 11:48:15 +0530

    Added captcha and formatted code

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function beforeSubmit(){
-    let outputdate = document.querySelector('.outputdate');
-    let inputdate = document.querySelector('.inputdate');
-    console.log('inputdate.value', inputdate.value);  //  string --> date (en_IN)
+function beforeSubmit() {
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log("inputdate.value", inputdate.value); //  string --> date (en_IN)
 
-    let formattedDate = new Date(inputdate.value).toLocaleDateString('en-IN');
-    outputdate.value = formattedDate;
+  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formattedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value,
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
